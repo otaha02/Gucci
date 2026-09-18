6519722b2c8ce21ef2b3162827db9c628d2a86f0
otaha02 <otaha02@gmail.com>
2026-09-18 11:38:08 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='french90-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
